333727d21ec235e4a6e0df62cb0780cce851337a
Hitesh Chandwani <hiteshchandwani98@gmail.com>
2021-09-17 04:23:39 +0530

    hot fix.

--- includes/ssp-firewall.php
+++ includes/ssp-firewall.php
@@ -532,7 +532,7 @@ function ssp_insert_post( $meta ) {
 	// limit requests entrires
 	$all_requests = wp_count_posts( 'ssp-firewall' );
 	if ( $all_requests->publish >= 10000 ) {
-		return
+		return;
 	}
 	// create post
 	$post_id = wp_insert_post(
